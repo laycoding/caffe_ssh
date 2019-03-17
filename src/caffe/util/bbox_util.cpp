135d126090a3bbda7da181cd834844d698123fda
Weidong Zhang <battle975444818@outlook.com>
2019-03-17 18:30:22 +0800

    init ssh multiboxloss

--- src/caffe/util/bbox_util.cpp
+++ src/caffe/util/bbox_util.cpp
@@ -635,54 +635,54 @@ void MatchBBox(const vector<NormalizedBBox>& gt_bboxes,
   }
 
   // Bipartite matching.
-  vector<int> gt_pool;
-  for (int i = 0; i < num_gt; ++i) {
-    gt_pool.push_back(i);
-  }
-  while (gt_pool.size() > 0) {
-    // Find the most overlapped gt and cooresponding predictions.
-    int max_idx = -1;
-    int max_gt_idx = -1;
-    float max_overlap = -1;
-    for (map<int, map<int, float> >::iterator it = overlaps.begin();
-         it != overlaps.end(); ++it) {
-      int i = it->first;
-      if ((*match_indices)[i] != -1) {
-        // The prediction already has matched ground truth or is ignored.
-        continue;
-      }
-      for (int p = 0; p < gt_pool.size(); ++p) {
-        int j = gt_pool[p];
-        if (it->second.find(j) == it->second.end()) {
-          // No overlap between the i-th prediction and j-th ground truth.
-          continue;
-        }
-        // Find the maximum overlapped pair.
-        if (it->second[j] > max_overlap) {
-          // If the prediction has not been matched to any ground truth,
-          // and the overlap is larger than maximum overlap, update.
-          max_idx = i;
-          max_gt_idx = j;
-          max_overlap = it->second[j];
-        }
-      }
-    }
-    if (max_idx == -1) {
-      // Cannot find good match.
-      break;
-    } else {
-      CHECK_EQ((*match_indices)[max_idx], -1);
-      (*match_indices)[max_idx] = gt_indices[max_gt_idx];
-      (*match_overlaps)[max_idx] = max_overlap;
-      // Erase the ground truth.
-      gt_pool.erase(std::find(gt_pool.begin(), gt_pool.end(), max_gt_idx));
-    }
-  }
+  // vector<int> gt_pool;
+  // for (int i = 0; i < num_gt; ++i) {
+  //   gt_pool.push_back(i);
+  // }
+  // while (gt_pool.size() > 0) {
+  //   // Find the most overlapped gt and cooresponding predictions.
+  //   int max_idx = -1;
+  //   int max_gt_idx = -1;
+  //   float max_overlap = -1;
+  //   for (map<int, map<int, float> >::iterator it = overlaps.begin();
+  //        it != overlaps.end(); ++it) {
+  //     int i = it->first;
+  //     if ((*match_indices)[i] != -1) {
+  //       // The prediction already has matched ground truth or is ignored.
+  //       continue;
+  //     }
+  //     for (int p = 0; p < gt_pool.size(); ++p) {
+  //       int j = gt_pool[p];
+  //       if (it->second.find(j) == it->second.end()) {
+  //         // No overlap between the i-th prediction and j-th ground truth.
+  //         continue;
+  //       }
+  //       // Find the maximum overlapped pair.
+  //       if (it->second[j] > max_overlap) {
+  //         // If the prediction has not been matched to any ground truth,
+  //         // and the overlap is larger than maximum overlap, update.
+  //         max_idx = i;
+  //         max_gt_idx = j;
+  //         max_overlap = it->second[j];
+  //       }
+  //     }
+  //   }
+  //   if (max_idx == -1) {
+  //     // Cannot find good match.
+  //     break;
+  //   } else {
+  //     CHECK_EQ((*match_indices)[max_idx], -1);
+  //     (*match_indices)[max_idx] = gt_indices[max_gt_idx];
+  //     (*match_overlaps)[max_idx] = max_overlap;
+  //     // Erase the ground truth.
+  //     gt_pool.erase(std::find(gt_pool.begin(), gt_pool.end(), max_gt_idx));
+  //   }
+  // }
 
-//  vector<int> gt_num_matched_anchor(num_gt, 1);
   switch (match_type) {
     case MultiBoxLossParameter_MatchType_BIPARTITE:
       // Already done.
+      LOG(FATAL)<<"ssh not support BipartiteMatching";
       break;
     case MultiBoxLossParameter_MatchType_PER_PREDICTION:
       // Get most overlaped for the rest prediction bboxes.
